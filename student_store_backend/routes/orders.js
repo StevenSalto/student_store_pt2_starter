452605fbbfcf0e57bfb1edec6dcf99349a90b27a
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-07-06 02:07:38 -0400

    order update

--- student_store_backend/routes/orders.js
+++ student_store_backend/routes/orders.js
@@ -5,18 +5,18 @@ const { post } = require("./store")
 const { requireAuthenticatedUser } = require("../middleware/security")
 
 router.get("/", requireAuthenticatedUser, async (req, res, next) => {
-    const user = res.locals.user
-    const orders = await Order.listOrdersForUser(user)
-    return res.status(200).json({ orders })
+    //const user = res.locals.user
+    //const orders = await Order.listOrdersForUser(user)
+    //return res.status(200).json({ orders })
 })
 
 router.post("/", requireAuthenticatedUser, async(req, res, next) => {
     try {   
-        const {user} = res.locals
-        const order = await Order.createOrder({ user, post: req.body })
-        return res.status(201).json({ order })
+        //const {user} = res.locals
+        //const order = await Order.createOrder({ user, post: req.body })
+        //return res.status(201).json({ order })
     } catch (error) {
-        next(error)
+        //next(error)
     }
 })
 
